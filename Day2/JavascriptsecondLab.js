4c1d0906912caefa31b2a2a24e3a1a7acd26f6da
hars123 <harshitshukla4513@gmail.com>
2025-04-08 12:05:19 +0530

    @Add@

--- Day2/JavascriptsecondLab.js
+++ Day2/JavascriptsecondLab.js
@@ -1,3 +1,34 @@
-console.log('3'+2+2);
-console.log(2+2+"3");
+var x = 5;
+var y = 2;
+console.log(!`The sum of x and y = ${x+y} `);
 
+
+let number = 5;
+
+if (number % 2 === 0) {
+  console.log(number + " is even.");
+} else {
+  console.log(number + " is odd.");
+}
+
+for (let i = 1; i <= 5; i++) {
+  if (i % 2 === 0) {
+    console.log(i + " is even.");
+  } else {
+    console.log(i + " is odd.");
+  }
+}
+
+
+// function implementation
+
+function checkOddEven(number) {
+    if (number % 2 === 0) {
+      console.log(number + " is Even");
+    } else {
+      console.log(number + " is Odd");
+    }
+  }
+  
+
+  checkOddEven(4);  
